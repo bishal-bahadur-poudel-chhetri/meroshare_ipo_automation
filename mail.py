0db50c1f30eb2ca992323c4122a0aad1a7d90157
bsal694 <chhetrirobot@gmail.com>
2022-09-05 10:11:42 +0545

    meroshare automation

---
+++ mail.py
@@ -0,0 +1,53 @@
+import smtplib
+import imghdr
+from email.mime.text import MIMEText 
+from email.mime.multipart import MIMEMultipart 
+from datetime import datetime
+import json
+
+
+
+def send_email(company_names):
+
+
+    login = "chhetrirobot@gmail.com" # paste your login 
+
+    sender_email = login 
+    receiver_email = company_names['email']
+    message = MIMEMultipart("alternative") 
+    message["Subject"] = f"Share Applied | {company_names['company']} | {company_names['type']}" 
+    message["From"] = login 
+    message["To"] = receiver_email
+    if company_names['error']:
+        error='' 
+
+
+
+    html =f"""\
+    <html>
+    <body>
+            <p>CompanyName: {company_names['company']}</p>
+            <p>ShareType: {company_names['type']}</p>
+            <p>Type: {company_names['share_type']}</p>
+            <p>Applied Time: {company_names['applied_time']}</p>
+
+            Thanks,<br>
+            Mero share Automation
+        </body>
+    </html>
+    """
+    part2 = MIMEText(html, "html") 
+    message.attach(part2)
+
+
+
+
+
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('chhetrirobot@gmail.com',"srnntujcflkbpqoo")
+    server.send_message(message)
+    server.quit()
+    print('Sent')
+
+    print(company_names['company'])
+
---
+++ s
@@ -0,0 +1,14 @@
+{
+
+
+    "user3":{
+      "DepositerParticipaterID":"NIC ASIA BANK LIMITED (13700)",
+      "CRN_Number":"01-01402401",
+      "UserName":"00086367",
+      "password":"@poudel560048",
+      "user_email":"bibekchhetri522@gmail.com",
+      "transaction_pin":"9843"
+   }
+
+ }
+ 
